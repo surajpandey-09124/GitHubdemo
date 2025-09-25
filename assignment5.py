9fcc7e2d9a82f17132ee691586ec20023e4de408
surajpandey-09124 <surajpandey09124@gmail.com>
2025-09-25 13:02:05 +0530

    Create assignment5.py

---
+++ assignment5.py
@@ -0,0 +1,9 @@
+def factorial(n):
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+    return result
+
+# Sample number
+num = 5
+print(f"Factorial of {num} is {factorial(num)}")
