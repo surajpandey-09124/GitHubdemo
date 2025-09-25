9b7bd468d8c1cbfb8442c86cbabf9897ae513811
surajpandey-09124 <surajpandey09124@gmail.com>
2025-09-25 13:03:08 +0530

    Create assignment6.py

---
+++ assignment6.py
@@ -0,0 +1,14 @@
+import math
+
+# Take input from user
+num = float(input("Enter a number: "))
+
+# Calculations using math module
+sqrt_num = math.sqrt(num)
+log_num = math.log(num)
+sin_num = math.sin(num)
+
+# Display results
+print(f"Square root of {num} is {sqrt_num}")
+print(f"Natural logarithm of {num} is {log_num}")
+print(f"Sine of {num} (radians) is {sin_num}")
